91e76deef91e6371e8dbfe8243f3ffd60962cf9b
f-zamanan <f.n.zamanan@gmail.com>
2025-05-25 15:33:14 +0300

    having trouble passing pets info

--- api/api.ts
+++ api/api.ts
@@ -1,18 +1,30 @@
 import axios from "axios";
 
-const FetchAllPets_url = "https://pets-react-query-backend.eapi.joincoded.com";
-
-const FetchOnePet_url = "https://pets-react-query-backend.eapi.joincoded.com";
-
+const Pets_url = "https://pets-react-query-backend.eapi.joincoded.com";
+interface NewPets {
+  name: string;
+  image: string;
+  type: string;
+  adopted: boolean;
+}
 const fetchAllPets = async () => {
-  const response = await axios.get(`${FetchAllPets_url}/pets`);
+  const response = await axios.get(`${Pets_url}/pets`);
   return response.data;
 };
 // const instance = axios.get(`${FetchAllPets_url}/pets`);
 // console.log(instance);
 const fetchOnePet = async (id: number) => {
-  const response = await axios.get(`${FetchOnePet_url}/pets/${id}`);
+  const response = await axios.get(`${Pets_url}/pets/${id}`);
   return response.data;
 };
 
-export { fetchAllPets, fetchOnePet };
+const addPetsPost = async (data: NewPets) => {
+  try {
+    const response = await axios.post(`${Pets_url}/pets`, data);
+    // return response.data;
+  } catch (error) {
+    console.error("Error posting data:", error);
+  }
+};
+
+export { fetchAllPets, fetchOnePet, addPetsPost };
